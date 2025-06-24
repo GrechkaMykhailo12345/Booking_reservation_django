d360635f2cf367f6ef9762a27f40f7f9f48c1382
GrechkaMykhailo12345 <yalta010709@gmail.com>
2025-06-24 14:31:04 +0300

    Додав логін

--- users_system/urls.py
+++ users_system/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from users_system import views
 
 urlpatterns = [
-    
+
     path('register/', views.register_page, name='register'),
+
+    path('login/', views.login_page, name='login'),
 
 ]
--- users_system/views.py
+++ users_system/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def register_page(request):
     form = UserCreationForm()
@@ -11,3 +12,19 @@ def register_page(request):
             login(request, user)
             return redirect('main')
     return render(request, 'users_system/register.html', context={"form": form})
+
+def login_page(request):
+    if request.method=='POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            messages.error(request, message="Неправильний логін і пароль")
+    else:
+        form = AuthenticationForm()
+        return render(request, 'users_system/login.html', context={"form": form})
